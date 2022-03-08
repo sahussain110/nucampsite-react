704d6b5e620edef81bd5f8d268a54c6302ae587e
Ash Hussain <ashguy110@gmail.com>
2022-03-08 00:26:35 -0600

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -20,37 +20,38 @@ class CampsiteInfo extends Component {
             return (
                 <div className="col-md-5 m-1">
                   <h4>Comments</h4>
-                  {comments.map(comment => (
-                    <div>
-                      <p>{comment.text}</p>
-                      <p>{comment.author}</p>
-                      <p>
-                        {new Intl.DateTimeFormat("en-US", {
+                  {comments.map(comment => {
+                    return(
+                        <div key={comment.id}>
+                      <p>{comment.text}<br />
+                         {comment.author}, {new Intl.DateTimeFormat("en-US", {
                           year: "numeric",
                           month: "short",
                           day: "2-digit",
-                        }).format(new Date(Date.parse(comment.date)))}
-                      </p>
+                        }).format(new Date(Date.parse(comment.date)))}</p>
                     </div>
-                  ))}
-                </div>
+                  );
+              })};
+            </div>
               );
-        } else {
-        }
+        } 
+        return <div />
     }
     
 
     render() {
-        if(this.props.campsite) {
-            return <div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-            </div>;
-        } else {
-            return <div></div>
-        }
-    }
+      if (this.props.campsite) {
+          return (
+              <div className="container">
+                  <div className="row">
+                      {this.renderCampsite(this.props.campsite)}
+                      {this.renderComments(this.props.campsite.comments)}
+                  </div>
+              </div>
+          );
+      }
+      return <div />;
+  }
 }
-
 
 export default CampsiteInfo;
