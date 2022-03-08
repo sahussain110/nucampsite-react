640ec764a5030eb2e4402ec83378c98da2f602ad
Ash Hussain <ashguy110@gmail.com>
2022-03-07 22:05:04 -0600

    Week 2 Assignment

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ import './App.css';
 import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
     render() {
         return (
             <div className="App">
                 <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
+                    <div className="container">
+                        <NavbarBrand href="/">NuCamp</NavbarBrand>
+                    </div>
                 </Navbar>
                 <Directory campsites={this.state.campsites} />
             </div>
         );
-    }
+    };
 }
 
 export default App;
