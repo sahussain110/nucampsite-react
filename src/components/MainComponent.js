c49211ce9699f3880e61c46502409a78ed5b3edd
Ash Hussain <ashguy110@gmail.com>
2022-03-11 22:30:02 -0600

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -34,7 +34,16 @@ class Main extends Component {
               partner={this.state.partners.filter(partner => partner.featured)[0]}
          />
         );
-    };
+    }
+
+    const CampsiteWithId = ({match}) => {
+      return (
+          <CampsiteInfo 
+              campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+              comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+          />
+      );
+  }
 
     return (
         <div>
@@ -42,6 +51,7 @@ class Main extends Component {
             <Switch>
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                 <Route exact path='/contactus' component={Contact} />
                 <Redirect to='/home' />
             </Switch>
