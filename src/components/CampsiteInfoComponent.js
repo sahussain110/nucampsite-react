b639ba68fec6f435333b25853e5f0180308aba92
Ash Hussain <ashguy110@gmail.com>
2022-03-24 01:11:02 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -49,8 +49,7 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -59,7 +58,6 @@ class CommentForm extends Component {
                 <Button color="success" outline onClick={this.toggleModal}>
                     <i className="fa fa-pencil fa-lg" /> Submit Comment
                 </Button>
-
                 <Modal
                     isOpen={this.state.isModalOpen}
                     toggle={this.toggleModal}
@@ -134,7 +132,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -155,7 +153,7 @@ function RenderComments({ comments }) {
                         </div>
                     );
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -182,7 +180,10 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments} 
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id} />
                 </div>
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -17,6 +18,10 @@ const mapStateToProps = state => {
       partners: state.partners,
       promotions: state.promotions
     };
+  }
+
+  const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
   }
 
   class Main extends Component {
@@ -38,6 +43,7 @@ const mapStateToProps = state => {
           <CampsiteInfo 
               campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
               comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+              addComment={this.props.addComment}
           />
       );
   }
@@ -59,4 +65,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
